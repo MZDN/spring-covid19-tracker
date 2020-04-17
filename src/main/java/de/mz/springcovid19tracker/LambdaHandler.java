51256cd82eb3bb9311d25290d7ea7bc5e6b6714c
MZ <m4zdn@outlook.com>
2020-04-17 12:58:23 +0200

    add aws lambda support

---
+++ src/main/java/de/mz/springcovid19tracker/LambdaHandler.java
@@ -0,0 +1,35 @@
+package de.mz.springcovid19tracker;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class LambdaHandler implements RequestStreamHandler {
+
+
+
+    private static SpringLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    static {
+        try {
+            handler = SpringLambdaContainerHandler.getAwsProxyHandler(SpringCovid19TrackerApplication.class);
+        } catch (ContainerInitializationException e) {
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring framework", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+
+        // just in case it wasn't closed by the mapper
+        outputStream.close();
+    }
+}
