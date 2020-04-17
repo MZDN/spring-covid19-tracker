e6c90b320ba80c9f9d5b0b76848243352afb74ba
MZ <m4zdn@outlook.com>
2020-04-17 12:59:07 +0200

    add aws lambda support

--- src/main/java/de/mz/springcovid19tracker/SpringCovid19TrackerApplication.java
+++ src/main/java/de/mz/springcovid19tracker/SpringCovid19TrackerApplication.java
@@ -35,7 +35,10 @@ package de.mz.springcovid19tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+import de.mz.springcovid19tracker.controllers.Covid19LocationController;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -43,6 +46,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@Configuration
+@Import({ Covid19LocationController.class })
 public class SpringCovid19TrackerApplication {
 
 	public static void main(String[] args) {
